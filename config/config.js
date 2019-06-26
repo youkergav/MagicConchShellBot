9aaf245567482ad4b784157b2138bc678fe39e37
Gavin Youker <youkergav@gmail.com>
2019-06-25 19:26:50 -0700

    Fixed config structure

--- config/config.js
+++ config/config.js
@@ -11,30 +11,14 @@ const defaults = {
     }
 }
 
-// Define environment configuration.
-var envConfigs = {
-    dev: {
-        server: {
-            port: process.env.DEV_SERVER_PORT || defaults.server.port
-        },
-        api: {
-            username: process.env.DEV_API_USERNAME,
-            password: process.env.DEV_API_PASSWORD,
-            authCode: process.env.DEV_API_AUTHCODE,
-            accessToken: process.env.DEV_API_ACCESSTOKEN
-        }
+// Define all configurations.
+module.exports = {
+    env: process.env.NODE_ENV,
+    server: {
+        port: parseInt(process.env.SERVER_PORT) || defaults.server.port
     },
-    test: {
-        app: {
-            server: process.env.TEST_SERVER_PORT || defaults.server.port
-        },
-        api: {
-            username: process.env.TEST_API_USERNAME,
-            password: process.env.TEST_API_PASSWORD,
-            authCode: process.env.TEST_API_AUTHCODE,
-            accessToken: process.env.TEST_API_ACCESSTOKEN
-        }
+    api: {
+        username: process.env.API_USERNAME,
+        password: process.env.API_PASSWORD
     }
 }
-
-module.exports = envConfigs[process.env.NODE_ENV] // Export the configuration.
