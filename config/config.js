ebc5bea497d590f30f82bb327704c1512387ce9c
Gavin Youker <youkergav@gmail.com>
2019-06-25 18:52:01 -0700

    Revert "Updated config structure"
    This reverts commit f1b51e8021bb659713d7c845723601fd1a2dadf1.

--- config/config.js
+++ config/config.js
@@ -2,24 +2,39 @@
 const path = require('path');
 const dotenv = require("dotenv")
 
-dotenv.config({ path: path.join(__dirname, "master.env") }); // Load the master environment variables.
-dotenv.config({ path: path.join(__dirname, process.env.NODE_ENV + ".env") }); // Load all the environment variables.
+dotenv.config({ path: path.join(__dirname, ".env") }); // Load the environment variables.
 
-// Define default configs.
+// Define default configurations.
 const defaults = {
     server: {
         port: 5500
     }
 }
 
-// Export all configs.
-module.exports = {
-    env: process.env.NODE_ENV,
-    server: {
-        port: process.env.SERVER_PORT || defaults.server.port
+// Define environment configuration.
+var envConfigs = {
+    dev: {
+        server: {
+            port: process.env.DEV_SERVER_PORT || defaults.server.port
+        },
+        api: {
+            username: process.env.DEV_API_USERNAME,
+            password: process.env.DEV_API_PASSWORD,
+            authCode: process.env.DEV_API_AUTHCODE,
+            accessToken: process.env.DEV_API_ACCESSTOKEN
+        }
     },
-    api: {
-        username: process.env.API_USERNAME,
-        password: process.env.API_PASSWORD
+    test: {
+        app: {
+            server: process.env.TEST_SERVER_PORT || defaults.server.port
+        },
+        api: {
+            username: process.env.TEST_API_USERNAME,
+            password: process.env.TEST_API_PASSWORD,
+            authCode: process.env.TEST_API_AUTHCODE,
+            accessToken: process.env.TEST_API_ACCESSTOKEN
+        }
     }
 }
+
+module.exports = envConfigs[process.env.NODE_ENV] // Export the configuration.
