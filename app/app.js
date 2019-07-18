368db23ef6b2d5de34c70c803ec3468c7c82c865
Gavin Youker <youkergav@gmail.com>
2019-07-17 22:02:52 -0700

    Reverted to functional project and merged

--- app/app.js
+++ app/app.js
@@ -1,11 +1,25 @@
 "use strict";
 
-const Server = require("./lib/server");
+// Import required modules.
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const router = require("./routes/general");
+const config = require("../config/config");
+const app = express();
 
-let server = new Server;
+// Connect to the database.
+let dbUser = encodeURIComponent(config.db.username);
+let dbPass = encodeURIComponent(config.db.password);
+let dbDatabase = encodeURIComponent(config.db.database);
+let mongoUri = `mongodb://${dbUser}:${dbPass}@${config.db.host}:${config.db.port}/${dbDatabase}`;
+mongoose.connect(mongoUri, { useNewUrlParser: true });
 
-server.initDb(); // Connect to the database.
-server.initRoutes(); // Use the router location.
-server.initViews(); // Set up the server views.
+app.use(router); // Use the router location.
 
-server.run();
+// Set up the views.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); // Set the static file location.
+
+app.listen(config.server.port); // Start the application on specified port.
